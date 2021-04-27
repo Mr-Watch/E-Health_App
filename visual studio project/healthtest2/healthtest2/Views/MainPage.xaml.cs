1b884cccb76da541eb750a07a00c384f69cd5ddf
Georgetsali99 <31631660+Georgetsali99@users.noreply.github.com>
2021-04-27 20:56:07 +0300

    the visual studio project so far

---
+++ visual studio project/healthtest2/healthtest2/Views/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml.Controls;
+using System.Net;
+using System.IO;
+using System.Windows;
+
+namespace healthtest2.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void searchbutton_click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            WebRequest request = HttpWebRequest.Create("http://127.0.0.1:5000/ath/api/v0.1/biomedlexicon/" + searchbar1.Text);
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string illnamejason = reader.ReadToEnd();
+            illname nameill = Newtonsoft.Json.JsonConvert.DeserializeObject<illname>(illnamejason);
+            testtext.change
+        }
+    }
+
+    public class illname
+    {
+        public string translation { get; set; }
+    }
+
+}
