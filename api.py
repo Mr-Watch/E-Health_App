adb91907636d8c8ee66dfcba645d155c7d4a9aeb
Mr-Watch <ssarantis2@gmail.com>
2021-05-24 18:36:05 +0300

    Added first draft of webmd api entry points

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 from biomedlexicon import search_biomedlexicon
 from scrapers.doctoranytime import get_doctors_list
-from healthatlas import get_hospitals_based_on_geoId,get_pharmacies_based_on_geoId,get_geoIds
+from healthatlas import get_hospitals_based_on_geoId, get_pharmacies_based_on_geoId, get_geoIds
 import symptom_api
 from farmakeia import get_pharmacies, get_url
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 api_version = '/ath/api/v0.1/'
@@ -20,15 +21,6 @@ def get_farmakeia_gr_pharmacies(pharmacy_type, district, sub_district):
     return jsonify(get_pharmacies(get_url(pharmacy_type, district, sub_district)))
 
 
-@app.route(api_version+'symptoms/', methods=['GET'])
-def _():
-    return jsonify(symptom_api.get_body_part_list())
-
-
-@app.route(api_version+'symptoms/get', methods=['GET'])
-def __():
-    return jsonify(symptom_api.get_body_url())
-
 @app.route(api_version+'doctoranytime/', methods=['GET'])
 def get_doctors():
     return jsonify(get_doctors_list('https://www.doctoranytime.gr/s/Aimatologos/attiki'))
@@ -37,24 +29,46 @@ def get_doctors():
 # Healthatlas api entry points
 
 @app.route(api_version+'healthatlas/hospitals/<string:geo_id>', methods=['GET'])
-def get_hospitals_based_on_geoId_flask(geo_id:str):
+def get_hospitals_based_on_geoId_flask(geo_id: str):
     return jsonify(get_hospitals_based_on_geoId(geo_id))
 
 
 @app.route(api_version+'healthatlas/pharmacies/<string:geo_id>', methods=['GET'])
-def get_pharmacies_based_on_geoId_flask(geo_id:str):
+def get_pharmacies_based_on_geoId_flask(geo_id: str):
     return jsonify(get_pharmacies_based_on_geoId(geo_id))
+
 
 @app.route(api_version+'healthatlas/', methods=['GET'])
 def get_geoIds_flask():
     return jsonify(get_geoIds())
 
 
+# Webmd api entry points
+
+@app.route(api_version+'webmd/symptoms/<string:body_section>/', methods=['GET'])
+def make_body_symptoms_single_api_request_flask(body_section: str):
+    return jsonify(symptom_api.make_body_symptoms_single_api_request(body_section))
 
 
+@app.route(api_version+'webmd/symptoms/<string:body_section>/<string:section_part>', methods=['GET'])
+def make_body_symptoms_api_request_flask(body_section: str, section_part: str):
+    return jsonify(symptom_api.make_body_symptoms_api_request(body_section, section_part))
 
 
-@app.route('/', methods=['GET'])
+@ app.route(api_version+'webmd/conditions/<string:age>/<string:gender>', methods=['POST'])
+def make_symptoms_api_request_flask(age: str, gender: str):
+    request_arguments = request.data.decode('utf-8')
+    return jsonify(symptom_api.make_symptoms_api_request(age, gender, request_arguments))
+
+
+@app.route(api_version+'webmd/body-part-ids/', methods=['GET'])
+def get_body_part_ids_flask():
+    return jsonify(symptom_api.get_body_part_ids())
+
+# General entry points
+
+
+@ app.route('/', methods=['GET'])
 def default_route():
     return jsonify({'api_version': '0.1'})
 
--- symptom_api.py
+++ symptom_api.py
@@ -1,27 +1,46 @@
-from webmd_enc_data import client_id, get_webmd_enc_data
-import requests
+from time import gmtime, strftime
 from pathlib import Path
+import requests
+import hashlib
+import base64
+import hmac
 import json
-""" enc_data_digest, gmt_time = get_webmd_enc_data()
-
-headers = {
-      'Accept': 'application/json',
-      'enc_data': enc_data_digest,
-      'client_id': client_id,
-      'timestamp': gmt_time,
-  }
 
 session = requests.session()
-page = session.get('https://symptoms.webmd.com/')
+session.get('https://symptoms.webmd.com/')
 
-response = session.get('https://symptoms.webmd.com/search/2/api/scbodytypeahead?q=&cache_2=true&gender=M&part=11&count=1000', headers=headers)
+client_id = 'e4e3f73a-0ceb-4d37-939e-90ddb1238360'
+secret_key = b'8720f366-3769-47e9-8156-187b9c6583b5'
 
-print(response.text) """
+
+def get_webmd_enc_data():
+    gmt_time = strftime("%a, %d %b %Y %I:%M:%S %Z", gmtime())
+    total_params = f"{{{{timestamp:{gmt_time},client_id:{client_id}}}}}"
+
+    total_params = bytearray(total_params, 'ascii')
+    enc_data_digest = hmac.new(
+        secret_key, total_params, hashlib.sha256).digest()
+
+    enc_data_bytes_64 = base64.b64encode(enc_data_digest)
+    enc_data = enc_data_bytes_64.decode('ascii')
+
+    return enc_data, gmt_time
+
+
+def get_request_headers():
+    enc_data_digest, gmt_time = get_webmd_enc_data()
+
+    headers = {
+        'Accept': 'application/json',
+        'enc_data': enc_data_digest,
+        'client_id': client_id,
+        'timestamp': gmt_time,
+    }
+    return headers
 
 
 def get_body_part_list():
-    data_path = Path('./')
-    with open(data_path / 'webmd_body_urls.json', 'r') as file:
+    with open('./webmd_body_part_ids.json', 'r') as file:
         json_data = json.load(file)
         file.close()
         return json_data
@@ -30,15 +49,72 @@ def get_body_part_list():
 data = get_body_part_list()
 
 
-def get_body_url():
+def get_body_part_ids():
     return data
 
 
 def get_body_part_url(body_section: str, section_part: str):
+    body_section_list = list(
+        filter(lambda x: x['body-section'] == body_section, data['data']))
+    body_section_id = body_section_list[0]['body-section-id']
 
-    print(len(data))
-    # body_section_list = list(filter(lambda x:x['body-section']==body_section,data))
-    # body_id = body_section_list[0]['body-id']
-    # part_id = list(filter(lambda x:x['section-parts']==section_part,body_section_list[0]['part']))
-    # print(body_section_list[0]['section-parts'][0])
-    # # url = f'https://symptoms.webmd.com/search/2/api/scbodytypeahead?q=&cache_2=true&gender=M&part={body_part}{part_id}&count=1000'
+    section_parts_list = body_section_list[0]['section-parts']
+    part_id = list(filter(lambda x: x['part'] == section_part, section_parts_list))[
+        0]['part-id']
+
+    url = f'https://symptoms.webmd.com/search/2/api/scbodytypeahead?q=&cache_2=true&gender=M&part={body_section_id}{part_id}&count=1000'
+
+    return url
+
+
+def get_single_body_part_url(body_section: str):
+    body_section_list = list(
+        filter(lambda x: x['body-section'] == body_section, data['data']))
+    body_section_id = body_section_list[0]['body-section-id']
+
+    url = f'https://symptoms.webmd.com/search/2/api/scbodytypeahead?q=&cache_2=true&gender=M&part={body_section_id}&count=1000'
+
+    return url
+
+
+def make_body_symptoms_single_api_request(body_section: str):
+    url = get_single_body_part_url(body_section)
+
+    response = session.get(url, headers=get_request_headers())
+
+    return response.json()
+
+
+def make_body_symptoms_api_request(body_section: str, section_part: str):
+    url = get_body_part_url(body_section, section_part)
+
+    response = session.get(url, headers=get_request_headers())
+
+    return response.json()
+
+
+def make_symptoms_api_request(age: str, gender: str, symptoms_list: str):
+    payload = json.dumps({
+        'request': {
+            'user': {
+                'age': age,
+                'gender': gender,
+                'zipcode': '',
+                'odata': '[CS]v1|294C0D80851D13B4-60000109600025DB[CE]'
+            },
+            'pregnancy': 2,
+            'refinement': [],
+            'symptoms': json.loads(symptoms_list),
+            'medications': [],
+            'prexistingdisease': []
+        },
+        'site_nm': 'core',
+        'maxconditions': 200,
+        'maxrefinements': 4,
+        'step': 0
+    })
+
+    response = session.post(
+        'https://symptoms.webmd.com/search/2/api/scapp/conditions', headers=get_request_headers(), data=payload)
+
+    return response.json()
